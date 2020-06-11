9d02c279161e68554cc076528df9c233eb974e86
Steimntz M. de Figueiredo <steimntz@gmail.com>
2020-06-11 17:55:45 -0400

    intial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.apache.kafka:kafka-clients:2.0.0")
+    implementation("com.twitter:hbc-core:2.2.0")
+    implementation("org.slf4j:slf4j-simple:1.7.30")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
